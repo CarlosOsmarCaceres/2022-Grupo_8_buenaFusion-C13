fef3b53a08348f6e25a1b19a1c762d255f43463b
Carlos Osmar Caceres <omarcaceres@live.com>
2022-04-08 16:09:48 -0300

    Unificacion de registro y acutualizacion de stilos login

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.get('/registro', (req, res) => {
 
 //Ruta index
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/src/views/header.html')
+  res.sendFile(__dirname + '/src/views/index.html')
 })
 //Ruta login
 
