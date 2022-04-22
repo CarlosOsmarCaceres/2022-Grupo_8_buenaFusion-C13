3805102cb777f90acca49bf28d7e3519a02b4985
Carlos Osmar Caceres <omarcaceres@live.com>
2022-04-22 06:01:21 -0300

    Se creo ususarios.json y actualizó Dailys

--- src/data/index.js
+++ src/data/index.js
@@ -2,5 +2,6 @@ const fs = require ('fs');
 const path= require('path');
 
 module.exports = {
-    getProduct: JSON.parse(fs.readFileSync(path.join(__dirname, "/productjson.json"),"utf-8"))
+    getProduct: JSON.parse(fs.readFileSync(path.join(__dirname, "/productjson.json"),"utf-8")),
+    getUsers: JSON.parse(fs.readFileSync(path.join(__dirname, "/usuarios.json"),"utf-8"))
 }
