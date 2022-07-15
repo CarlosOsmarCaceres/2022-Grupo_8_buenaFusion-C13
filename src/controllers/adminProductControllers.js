c4dbaca904a0eed8123f645e58419a2d128770dc
Carlos Osmar Caceres <omarcaceres@live.com>
2022-07-15 00:16:05 -0300

    Validation result a admiProductController

--- src/controllers/adminProductControllers.js
+++ src/controllers/adminProductControllers.js
@@ -1,5 +1,7 @@
 //const { getProduct, escribirJson } = require("../data");
 const { Product, Category } = require("../database/models");
+const { validationResult } = require('express-validator');
+
 
 module.exports = {
   productList: (req, res) => {
@@ -21,15 +23,30 @@ module.exports = {
     });
   },
   productCreate: (req, res) => {
-    Product.create({
-        ...req.body,
-        image: req.file ? req.file.filename : "default-image.png",
-        stock: req.body.stock ? true : false,
-    })
-    .then(() => {
-        res.redirect("/administrador/productos");
-    })
+    let errors =validationResult(req);
+    //res.send(errors)
+    if(errors.isEmpty()){
+      Product.create({
+          ...req.body,
+          image: req.file ? req.file.filename : "default-image.png",
+          stock: req.body.stock ? true : false,
+      })
+      .then(() => {
+          res.redirect("/administrador/productos");
+      })
     
+      .catch(error => res.send(error))
+    } else{
+    Category.findAll().then((categorias) => {
+      res.render("admin/products/addProduct", {
+        title: "Agregar Productos",
+        session: req.session,
+        errors: errors.mapped(),
+        old: req.body,
+        categorias,
+      });
+    });
+    }
   },
   productEdit: (req, res) => {
         let producto = Product.findByPk(req.params.id)
@@ -45,19 +62,38 @@ module.exports = {
         })
   },
   productoEditar: (req, res) => {
-    let producto = Product.findByPk(req.params.id)
-    Product.update({
-        ...req.body,
-        image: req.file ? req.file.filename : producto.image,
-        stock: req.body.stock ? true : false,
-    },
-    {
-        where : { id : req.params.id}
-    })
-    .then(() => {
-        res.redirect("/administrador/productos");
-    })
-    .catch(errors => console.log(errors))
+
+    let errors = validationResult(req);
+        //res.send(errors)
+    if(errors.isEmpty()){
+
+      let producto = Product.findByPk(req.params.id)
+      Product.update({
+          ...req.body,
+          image: req.file ? req.file.filename : producto.image,
+          stock: req.body.stock ? true : false,
+        },
+        {
+            where : { id : req.params.id}
+        })
+        .then(() => {
+            res.redirect("/administrador/productos");
+          })
+          .catch(errors => console.log(errors))
+    } else {
+      let producto = Product.findByPk(req.params.id)
+      let categorias = Category.findAll()
+      Promise.all([producto, categorias])
+      .then(([producto, categorias]) => {
+        res.render("admin/products/editProduct", {
+          title: "Editar Productos",
+          producto,
+          categorias,
+          errors: errors.mapped(),
+          session: req.session,
+        });
+      })
+    }
   },
   productDelete: (req, res) => {
     Product.destroy({
