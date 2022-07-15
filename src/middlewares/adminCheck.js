70d666d5013af8c4ae167b0f87b991e32ccaf442
Carlos Osmar Caceres <omarcaceres@live.com>
2022-07-15 00:28:22 -0300

    arreglos de estilos  y cargade de imagen

--- src/middlewares/adminCheck.js
+++ src/middlewares/adminCheck.js
@@ -1,5 +1,5 @@
 const adminCheck = (req, res, next) => {
-    if (req.session.usuario.rol === "ADMIN"){
+    if (req.session.usuario.roluser_id == "1"){
         next ()
      } else {
         res.send( "No tienen permiso para ingresa,Mati y Jona esto ya lo sabemos  xD")
